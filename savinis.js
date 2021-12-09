e5534aa5c181a86c13a33c99603dc33e796a2fac
gabrielbrito23 <gabrielcarlosdealmeidabrito@gmail.com>
2021-12-09 11:13:35 -0300

    Código de agendamentos de eventos

---
+++ savinis.js
@@ -0,0 +1,33 @@
+// primeira parte data ///
+var data = ["Digite a data que quer reservar:"]
+if (data.length > 10){
+    console.log("Data inválida")
+}else{
+    console.log("Data disponível")
+}
+
+// Segunda parte idade //
+var idade = ["digite sua idade:"]
+if  (idade.leght < 18){
+    console.log("Idade inválida")
+}else{
+    console.log("Idade suficiente")
+}
+
+// Terceira parte pessoas //
+var pessoas = ["Digite a quantidade de pessoas:"] 
+var palestrantes = ["Digite a quantidade de palestrantes "]
+let soma = 100
+if (soma.lenght < 100){
+    console.log("Quantidade inválida")
+}else{
+    console.log("Quantidade Aprovada")
+}
+
+// Quarta parte Resultado //
+var resultado = soma
+if(soma.leght< 100){
+    console.log("Evento recusado")
+} else{
+    console.log("Evento aprovado")
+}
